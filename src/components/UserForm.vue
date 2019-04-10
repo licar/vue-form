a95c4874951060b9aa9d17361bcc2a0218649b11
kim.ranim <evgeniy.polikarpov95@gmail.com>
2019-04-10 16:23:22 +0300

    Edited post

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -3,11 +3,11 @@
     <form @submit="checkForm">
       <div class="form-group">
         <label>Имя:</label>
-        <input id="name" class="form-control" v-model="form.name"  type="text">
+        <input id="name" class="form-control" v-model="user.name"  type="text">
       </div>
       <div class="form-group">
         <label>Вид занятости:</label>
-        <select id="employment_type" class="form-control" v-model="form.employment_type">
+        <select id="employment_type" class="form-control" v-model="user.employment_type">
           <option v-for="option in options" v-bind:value="option.id">
             {{ option.text }}
           </option>
@@ -15,18 +15,21 @@
       </div>
       <div class="form-group">
           <label>Пол:</label>
-          <label class="radio-inline"><input type="radio" v-bind:value="0" v-model="form.sex">М</label>
-          <label class="radio-inline"><input type="radio" v-bind:value="1" v-model="form.sex">Ж</label>
+          <label class="radio-inline"><input type="radio" v-bind:value="0" v-model="user.sex">М</label>
+          <label class="radio-inline"><input type="radio" v-bind:value="1" v-model="user.sex">Ж</label>
       </div>
       <div class="form-group">
         <label>Хобби: </label>
-        <textarea class="form-control" v-model="form.hobbies">
+        <textarea class="form-control" v-model="user.hobbies">
         </textarea>
       </div>
       <div class="form-group form-check">
         <label class="form-check-label">
-          <input class="form-check-input" type="checkbox" v-model="form.married"> Женат/замужем
+          <input class="form-check-input" type="checkbox" v-model="user.married"> Женат/замужем
         </label>
+      </div>
+      <div  v-if="errors.length > 0" class="alert alert-danger">
+        <strong>Danger!</strong> {{this.errors[0]}} 
       </div>
       <button type="submit" class="btn btn-primary">Отправить</button>
     </form>
@@ -40,7 +43,7 @@ export default {
   name: 'UserForm',
   data() {
     return {
-      form:{
+      user:{
         name: '',
         employment_type: 0,
         sex: 0,
@@ -57,27 +60,27 @@ export default {
   },
   methods: {
     async checkForm(e) {
-      await this.sendForm();
       this.errors = [];
       
-      if (this.form.name.length <= 0) {
+      if (this.user.name.length <= 0) {
         this.errors.push('Требуется указать имя');
       }
 
-      if (this.form.hobbies.length <= 0) {
+      if (this.user.hobbies.length <= 0) {
         this.errors.push('Поле хобби должно быть заполненно');
       }
 
-      if (!this.errors){
+      if (!this.errors.length){
+        await this.addUser();
         return true;
       }
 
       e.preventDefault();
     },
     
-    async sendForm () {
+    async addUser () {
       try{
-        const { result } = await axios.post(`${process.env.VUE_APP_SERVER_URL}/api`, this.form);
+        const result = await axios.post(`${process.env.VUE_APP_SERVER_URL}/api`, this.user);
       }catch(err){
         console.log(err);
       }
